10341020ba4ca0bef26f3b78cc700651deba0a42
Daniel Thornton <dshaggy28@yahoo.com>
2013-12-05 17:13:12 -0800

    game now one file

--- computermayhem/computerMayhemRooms.py
+++ computermayhem/computerMayhemRooms.py
@@ -1,3 +1,14 @@
+import sys 
+
+def end():
+	print("The end, You have done well")
+	playAgain = input('play again(y/n)').lower()
+	if playAgain == 'y' :
+		start()
+	elif playAgain == 'n':
+		sys.exit()
+	else: 
+		print("what in the world are you doing here")
 
 def die():
 	print("You should not be here, something is wrong with")
@@ -67,7 +78,7 @@ def toilet():
 	print(" Though it was cathartic to hit something and")
 	print(" see it smash into pieces, yes")
 	print(" You start to feel better")
-	end()
+	#end()
 	
 def vanity():
 	print("How could you ever think of ever hitting your own vanity.")
@@ -157,6 +168,52 @@ def sink():
 	print("You loose.")
 	
 # end of the first version of the game
+
+def start():
+	print("ComputerMayhem Version 1.0, \n" )
+	intro()
+
+def intro():
+	print('You are a digurntled office worker who cant take it anymore.') 
+	print('You sit all day in front of your computer')
+	print('doing nothing and then you come home and sit in your') 
+	print('computer until its time to go to be and do')
+	print('everything over again. And you are tired of it.')
+	print('You are Alaina and since you cant take your anger')
+	print('Out on your work computer. You decide to destroy')
+	print('You home computers.\n')
+	condo()
+
+def condo():
+	print('You race home in your sports car. To your')
+	print('fancy new condo to do some damage to your Electronics')
+	print('Where do you want to go')
+	print("bd) bedroom")
+	print('l) livingroom')
+	print('b) bathroom' )
+	print('k) kitchen')
+	#print('g) game room')
+	selectARoom = input('>')
+	selectRoom(selectARoom)
+	
+	
+	
+def selectRoom(whichRoom):
+	if whichRoom == 'bd':
+		bedRoom()
+	elif whichRoom == 'l':
+		livingRoom()
+	elif whichRoom == 'b':
+		bathRoom()
+	elif whichRoom == 'k':
+		kitchen()
+	#elif whichRoom == 5:
+		#computerMayhemRooms.gameRoom()
+	else:
+		print('Invalid selection please select 1=5, thank you.')
+		end()
+start()
+
 	
 
 	
